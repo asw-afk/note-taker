07be32c3c675042cbefb3796e004dd622b19b247
asw <annasilverwolff@gmail.com>
2024-06-23 00:02:38 -0400

    Server runs, but doesnt show the correct page

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const PORT = 3001;
+// const noteApp = require("./db/db.json");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// app.get('/api', (req, res) => res.json(termData));
+
+//Console log to display server is running
+app.listen(PORT, () => {
+  console.log(`app listening for http://localhost:${PORT}`);
+});
